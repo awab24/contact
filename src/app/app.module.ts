865469475e23ef67b6a3c9d000aaa112a8001703
Awab Hassan <awab1hassan1@gmail.com>
2024-08-05 11:55:54 +0300

    Sds

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddContactComponent } from './add-contact/add-contact.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddContactComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
